1f95ba9f0b5a4fff592fecc5fdb2bbe5877d2e0b
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-10-05 20:02:46 +0530

    Wrap logic in lox class

--- src/lox/lox.py
+++ src/lox/lox.py
@@ -5,68 +5,67 @@ from src.lox.interpreter import Interpreter
 from src.lox.parser import Parser
 from src.lox.scanner import Scanner
 
-had_errors = False
-had_runtime_errors = False
 
+class Lox:
+    def __init__(self):
+        self.had_errors = False
+        self.had_runtime_errors = False
 
-def run(code: str):
-    scanner = Scanner(code)
-    tokens = scanner.scan_tokens()
+    def run(self, code: str):
+        scanner = Scanner(code)
+        tokens = scanner.scan_tokens()
 
-    global had_errors
-    global had_runtime_errors
+        if len(scanner.errors) > 0:
+            print_errors(parser.errors)
+            self.had_errors = True
+            return
 
-    if len(scanner.errors) > 0:
-        print_errors(parser.errors)
-        had_errors = True
-        return
+        parser = Parser(tokens)
+        ast = parser.parse()
 
-    parser = Parser(tokens)
-    ast = parser.parse()
+        if len(parser.errors) > 0:
+            self.had_errors = True
+            print_errors(parser.errors)
+            return
 
-    if len(parser.errors) > 0:
-        had_errors = True
-        print_errors(parser.errors)
-        return
+        interpreter = Interpreter()
+        result = interpreter.interpret(ast)
 
-    interpreter = Interpreter()
-    result = interpreter.interpret(ast)
+        if interpreter.has_error:
+            print_errors(interpreter.errors)
+            self.had_runtime_errors = True
+            return
 
-    if interpreter.has_error:
-        print_errors(interpreter.errors)
-        had_runtime_errors = True
-        return
+        print(stringify(result))
 
-    print(stringify(result))
+    def run_file(self, file: str):
+        with open(file, "r", encoding="utf-8") as _file:
+            self.run(_file.read())
+            if self.had_errors:
+                sys.exit(65)
+            if self.had_runtime_errors:
+                sys.exit(70)
 
-
-def run_file(file: str):
-    with open(file, "r", encoding="utf-8") as _file:
-        run(_file.read())
-        if had_errors:
-            sys.exit(65)
-        if had_runtime_errors:
-            sys.exit(70)
-
-
-def start_repl():
-    while True:
-        line = input("> ")
-        if line is None:
-            break
-        run(line)
+    def start_repl(self):
+        while True:
+            line = input("> ")
+            if line is None:
+                break
+            self.run(line)
 
 
 def main(args: list[str]):
     arg_size = len(args)
 
+    lox = Lox()
+
     if arg_size > 1:
         print("Usage: plox [script]")
         sys.exit(64)
     elif arg_size == 1:
-        run_file(args[0])
+        lox.run_file(args[0])
     else:
-        start_repl()
+        lox.start_repl()
 
 
 if __name__ == "__main__":
